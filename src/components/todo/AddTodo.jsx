7fee7137ff6aaaf7fea24b643293a18902be9e63
abdullah-shah <abi.salim5@gmail.com>
2023-07-28 11:28:34 +0500

    style: added media query for mobile screens

--- src/components/todo/AddTodo.jsx
+++ src/components/todo/AddTodo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AddTodo({todoItem, handleChange, handleSubmit, isTodoTouched}) {
   return (
     <>
-    <h1 style={{textAlign:'center', background:"#f0f0f0"}}>Todo List</h1>
+    <h1 style={{textAlign:'center', background:"#f0f0f0"}}>Todo App</h1>
     <form noValidate>
             <label htmlFor="todoText">Add New Todo</label>
             <input
